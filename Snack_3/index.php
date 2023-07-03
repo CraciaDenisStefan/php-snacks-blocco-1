6512885ef0f3c0acaef052d108d6c690ea367b67
CraciDenisStefan <crdenis13@gmail.com>
2023-07-03 20:24:38 +0200

    Snack 3 completo

---
+++ Snack_3/index.php
@@ -0,0 +1,68 @@
+<?php
+
+$posts = [
+
+    '10/01/2019' => [
+        [
+            'title' => 'Post 1',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 1'
+        ],
+        [
+            'title' => 'Post 2',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 2'
+        ],
+    ],
+    '10/02/2019' => [
+        [
+            'title' => 'Post 3',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 3'
+        ]
+    ],
+    '15/05/2019' => [
+        [
+            'title' => 'Post 4',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 4'
+        ],
+        [
+            'title' => 'Post 5',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 5'
+        ],
+        [
+            'title' => 'Post 6',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 6'
+        ]
+    ],
+];
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Snack 3</title>
+</head>
+<body>
+<?php foreach($posts as $index => $post) { ?>          
+    <h3><?php echo $index ?></h3>
+        <ul>
+        <?php foreach ($post as $post_user){ ?>
+            <li>
+                <?php echo $post_user['title'] ?></br>
+                        
+                <?php echo $post_user['author'] ?></br>
+                            
+                <?php echo $post_user['text'] ?></br>
+                                
+            </li>
+        <?php }?>
+        </ul>
+<?php } ?>
+</body>
+</html>
